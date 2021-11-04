cd37b0d50623b57cb89b0f6c9ebd88c6bc888b50
IJLAL AHMAD <ijlalahmad845@gmail.com>
2021-11-04 12:07:08 +0530

    Circular Stack(Array) IsEmpty & IsFull Operations

---
+++ Circular Stack (Array)/IsEmpty.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int IsEmpty(int arr[], int f,int r,int n)
+{
+    if (f==-1)
+        return 1;
+    else
+        return 0;
+
+}
+
+int main()
+{
+    system("cls");
+
+    int arr[] = {10, 23, 45, 32, 54};
+    int mSize = sizeof(arr) / sizeof(arr[0]);
+
+    //r is for insertion at end and overwrites succeeding values
+    //f follows r in that case
+    //see POP for more detail about is empty
+    int f = 4, r = 4;//at 4th index right now
+
+    cout << IsEmpty(arr,f,r, mSize);
+}
