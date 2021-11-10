020ed0557dd657cbdd1b875cf137b46309f28118
IJLAL AHMAD <ijlalahmad845@gmail.com>
2021-11-10 15:04:40 +0530

    Whole generic queue class (Array) with all operations

---
+++ Complete Classes/Queue Class (Array)/QueueGeneric.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+using namespace std;
+
+//generic queue class implementation using array
+template <typename T>
+class Queue
+{
+private:
+    int front, rear;
+    int size;
+    T *arr;
+
+public:
+    //constructor for initialising size , front and rear
+    Queue(int size)
+    {
+        front = rear = -1;
+        this->size = size;
+        arr = (T *)malloc(size * sizeof(T));
+    }
+
+    //function for push element in the queue
+    void enqueue(T element)
+    {
+        if (front == -1 && rear == -1)
+        {
+            front = rear = 0;
+            arr[rear] = element;
+        }
+        else if (rear < size - 1)
+        {
+            rear++;
+            arr[rear] = element;
+        }
+        else
+            cout << "\nQueue Overflow!!!\n";
+    }
+
+    //function for removing element from the queue
+    T dequeue()
+    {
+        if (front > rear || front == -1)
+        {
+            cout << "\nQueue Underflow!!!\n";
+            return -1;
+        }
+        else
+        {
+            T element = arr[front];
+            front++;
+
+            if (front > rear)
+                front = rear = -1;
+            return element;
+        }
+    }
+
+    //function for getting front element from the queue
+    T Front()
+    {
+        if (front == -1)
+            return -1;
+        else
+            return arr[front];
+    }
+
+    //function for getting rear element from the queue
+    T Rear()
+    {
+        if (rear == -1)
+            return -1;
+        else
+            return arr[rear];
+    }
+
+    //function for checking if queue is empty or not
+    int isEmpty()
+    {
+        if (front == -1 && rear == -1)
+            return 1;
+        else
+            return 0;
+    }
+
+    //function fot checking if queue is full or not
+    int isFull()
+    {
+        if (rear == size - 1)
+            return 1;
+        else
+            return 0;
+    }
+
+    //function to display whole queue
+    void display()
+    {
+        if (rear == -1 && front == -1)
+            cout << "[ ]";
+        else
+        {
+            cout << "\n[ ";
+
+            for (int i = front; i <= rear; i++)
+            {
+                cout << arr[i];
+                if (i != rear)
+                    cout << " | ";
+            }
+
+            cout << " ]";
+        }
+    }
+
+    //function for getting size of queue
+    int Size()
+    {
+        if (rear == -1 && front == -1)
+            return 0;
+        return rear - front + 1;
+    }
+};
+
+int main()
+{
+    system("cls");
+
+    //generic queue class objects declaration
+    Queue<int> q1(5);
+    q1.enqueue(10);
+    q1.enqueue(20);
+    q1.enqueue(30);
+    q1.enqueue(40);
+    q1.enqueue(50);
+
+    q1.display();
+
+    Queue<float> q2(5);
+    q2.enqueue(10.51);
+    q2.enqueue(20.2);
+    q2.enqueue(30.65);
+    q2.enqueue(40.21);
+    q2.enqueue(50.32);
+
+    q2.display();
+
+    Queue<char> q3(4);
+    q3.enqueue('a');
+    q3.enqueue('b');
+    q3.enqueue('c');
+    q3.enqueue('d');
+
+    q3.display();
+}
