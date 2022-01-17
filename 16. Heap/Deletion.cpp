6ee160a2ad1ba3dc0e1d8863b991b9962bab3cf1
IJLAL AHMAD <ijlalahmad845@gmail.com>
2022-01-17 23:06:55 +0530

    added Heap insertion and deletion operations

---
+++ 16. Heap/Deletion.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+void display(int arr[], int size)
+{
+    //array indexing is from 1..size
+    for (int i = 1; i <= size; i++)
+        cout << arr[i] << " ";
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+//function to insert in max heap
+int RemoveMaxHeap(int arr[], int *size)
+{
+    if ((*size) == 1)
+    {
+        cout << "Heap is Empty!!!" << endl;
+        return -1;
+    }
+
+    int element = arr[1];
+    arr[1] = arr[*size];
+    (*size)--;
+
+    int i = 1;
+    int j = 2 * i;
+
+    while (j <= *size)
+    {
+        if (arr[j] > arr[j + 1])
+        {
+            if (arr[i] < arr[j])
+                swap(&arr[i], &arr[j]);
+            i = j;
+        }
+        else
+        {
+            if (arr[i] < arr[j + 1])
+                swap(&arr[i], &arr[j + 1]);
+            i = j + 1;
+        }
+
+        j = 2 * i;
+    }
+
+    return element;
+}
+
+//function to insert in min heap
+int RemoveMinHeap(int arr[], int *size)
+{
+    if ((*size) == 1)
+    {
+        cout << "Heap is Empty!!!" << endl;
+        return -1;
+    }
+
+    int element = arr[1];
+    arr[1] = arr[*size];
+    (*size)--;
+
+    int i = 1;
+    int j = 2 * i;
+
+    while (j <= *size)
+    {
+        if (arr[j] < arr[j + 1])
+        {
+            if (arr[i] > arr[j])
+                swap(&arr[i], &arr[j]);
+            i = j;
+        }
+        else
+        {
+            if (arr[i] > arr[j + 1])
+                swap(&arr[i], &arr[j + 1]);
+            i = j + 1;
+        }
+
+        j = 2 * i;
+    }
+
+    return element;
+}
+
+int main()
+{
+    system("cls");
+
+    int maxHeap[] = {0, 22, 21, 20, 5, 15, 10, 19};
+    int size;
+
+    //heap starts from index 1, so size is 1 less than original size
+    size = sizeof(maxHeap) / sizeof(maxHeap[0]) - 1;
+
+    cout << "Removed Value: " << RemoveMaxHeap(maxHeap, &size) << endl;
+    cout << "Removed Value: " << RemoveMaxHeap(maxHeap, &size) << endl;
+    cout << "Removed Value: " << RemoveMaxHeap(maxHeap, &size) << endl;
+    cout << "Max heap: ";
+    display(maxHeap, size);
+
+    int minHeap[] = {0, 5, 10, 20, 15, 21, 22, 25};
+    //heap starts from index 1, so size is 1 less than original size
+    size = sizeof(minHeap) / sizeof(minHeap[0]) - 1;
+
+    cout << endl
+         << endl;
+    cout << "Removed Value: " << RemoveMinHeap(minHeap, &size) << endl;
+    cout << "Removed Value: " << RemoveMinHeap(minHeap, &size) << endl;
+    cout << "Removed Value: " << RemoveMinHeap(minHeap, &size) << endl;
+    cout << "Min heap: ";
+    display(minHeap, size);
+}
---
+++ 16. Heap/Insertion.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+void display(int arr[], int size)
+{
+    //arr indexing is from 1..size
+    for (int i = 1; i <= size; i++)
+        cout << arr[i] << " ";
+}
+
+//function to insert in max heap
+void InsertMaxHeap(int element, int arr[], int *size, int mSize)
+{
+    if (*size >= mSize)
+    {
+        cout << "Size Overflow!!!" << endl;
+        return;
+    }
+
+    int i = *size + 1;
+    while (i > 0)
+    {
+        if (element < arr[i / 2])
+            break;
+
+        arr[i] = arr[i / 2];
+        i /= 2;
+    }
+    arr[i] = element;
+    (*size)++;
+}
+
+//function to insert in min heap
+void InsertMinHeap(int element, int arr[], int *size, int mSize)
+{
+    if (*size >= mSize)
+    {
+        cout << "Size Overflow!!!" << endl;
+        return;
+    }
+
+    int i = *size + 1;
+    while (i > 1)
+    {
+
+        if (element > arr[i / 2])
+            break;
+
+        arr[i] = arr[i / 2];
+        i /= 2;
+    }
+    arr[i] = element;
+    (*size)++;
+}
+
+int main()
+{
+    system("cls");
+
+    int mSize = 6;
+    int arr[mSize + 1];
+    int size = 0;
+
+    InsertMinHeap(10, arr, &size, mSize);
+    InsertMinHeap(5, arr, &size, mSize);
+    InsertMinHeap(20, arr, &size, mSize);
+    InsertMinHeap(15, arr, &size, mSize);
+    InsertMinHeap(21, arr, &size, mSize);
+    InsertMinHeap(22, arr, &size, mSize);
+
+    display(arr, size);
+}
