0eabb1790b98b8ae67d6ecf8de1b3429387611f9
IJLAL AHMAD <ijlalahmad845@gmail.com>
2022-01-19 01:18:59 +0530

    added TreeSort

---
+++ 17. Sorting Algorithms/TreeSort.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+};
+
+//function for inserting element in BST
+node *InsertBST(int element, node *root)
+{
+    node *ptr = NULL;
+    if (root == NULL)
+    {
+        ptr = new node;
+        ptr->data = element;
+        ptr->left = ptr->right = NULL;
+
+        return ptr;
+    }
+
+    // '<=' for allowance for duplicate keys for sorting purpose only
+    // otherwise BST should not have duplicate keys
+    if (element <= root->data)
+        root->left = InsertBST(element, root->left);
+    else if (element > root->data)
+        root->right = InsertBST(element, root->right);
+
+    return root;
+}
+
+//function for extracting keys from BST in sorted way using inorder traversal
+void SortByInorder(int arr[], node *root)
+{
+    //static variable for singleton pattern
+    static int index = 0;
+
+    if (root != NULL)
+    {
+        if (root->left != NULL)
+            SortByInorder(arr, root->left);
+
+        arr[index++] = root->data;
+
+        if (root->right != NULL)
+            SortByInorder(arr, root->right);
+    }
+}
+
+//function for sorting array using Tree Sort (Binary Search Tree)
+int *TreeSort(int arr[], int n)
+{
+    node *BST = NULL;
+
+    for (int i = 0; i < n; i++)
+        BST = InsertBST(arr[i], BST);
+
+    SortByInorder(arr, BST);
+
+    return arr;
+}
+
+int main()
+{
+    system("cls");
+    int arr[] = {2, 5, 41, 6, 8, 25, 41, 5, 63, 1, 4, 6, 2, 1, 1, 52, 74, 96, 20, 54, 4, 6, 52, 45, 89, 63, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int *ptr;
+
+    ptr = TreeSort(arr, size);
+
+    for (int i = 0; i < size; i++)
+        cout << ptr[i] << " ";
+}
